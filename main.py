e2de6d81d19492138505824906afa2571dd1646f
devmentality <nikita.volnov.1999@mail.ru>
2021-08-21 14:44:01 +0500

    manager sign in, sign up

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 import pymongo
 import os
+from pydantic import BaseModel
+from models import User
+import hashlib
 
 app = FastAPI()
 
@@ -9,12 +12,44 @@ DB_USER = os.environ['KICKIN_DB_USER']
 DB_PASSWORD = os.environ['KICKIN_DB_PASSWORD']
 
 client = pymongo.MongoClient(
-    f'mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.4cnae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    f'mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.4cnae.mongodb.net/app?retryWrites=true&w=majority'
 )
+db = client['app']
+
 
 @app.get('/')
 def hello():
     return {'message': 'Hello, World!'}
+
+
+class ManagerCredentials(BaseModel):
+    login: str
+    password: str
+
+
+@app.post('/manager_sign_up', response_model=User)
+def manager_sign_up(data: ManagerCredentials):
+    # TODO: handle if user exists
+    password_hash = hashlib.sha256(data.password.encode('ascii')).hexdigest()
+    user = User.parse_obj(
+        {
+            'login': data.login,
+            'password_hash': password_hash,
+            'role': 'manager'
+        })
+
+    obj_id = db.users.insert_one(user.dict()).inserted_id
+    return db.users.find_one({'_id': obj_id})
+
+
+@app.post('/manager_login', response_model=User)
+def manager_login(data: ManagerCredentials):
+    password_hash = hashlib.sha256(data.password.encode('ascii')).hexdigest()
+    user = User.parse_obj(db.users.find_one({'login': data.login}))
+
+    if user.password_hash == password_hash:
+        return user
+    raise HTTPException(status_code=401, detail='wrong credentials')
 
 
 if __name__ == "__main__":
