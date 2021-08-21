5de4a35d621f0b657e2069fe71694c3966d8ca0a
devmentality <nikita.volnov.1999@mail.ru>
2021-08-21 12:18:30 +0500

    init client

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
+import pymongo
+import os
 
 app = FastAPI()
 
+DB_USER = os.environ['KICKIN_DB_USER']
+DB_PASSWORD = os.environ['KICKIN_DB_PASSWORD']
+
+client = pymongo.MongoClient(
+    f'mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.4cnae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+)
 
 @app.get('/')
 def hello():
